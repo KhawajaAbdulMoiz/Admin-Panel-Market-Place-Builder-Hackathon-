f612e76c1a5215f6a44e0757450dcef615bfe7d3
KhawajaAbdulMoiz <khawajaahmedraza4@gmail.com>
2025-02-08 00:05:37 +0500

    Resolved

--- admin-dashboard/app/page.tsx
+++ admin-dashboard/app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Admin Dashboard</h1>
+      <h1>Change Your Path to "admin/dashboard"</h1>
     </div>
   );
 }
--- admin-dashboard/sanity-nextjs/src/sanity/lib/client.ts
+++ admin-dashboard/sanity-nextjs/src/sanity/lib/client.ts
@@ -1,13 +1,12 @@
-
-import { createClient } from '@sanity/client';
+import { createClient } from 'next-sanity';
 
 export const sanityClient = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, 
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,       
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
-  token:process.env.SANITY_API_TOKEN || 'skLccAvpl4urQgcGq2gTutgbklAFxjEZBLYht2DR5oOVCv0lmLSDzOGgDriyZ5MscBsOIRTt5FQCHgjKK8IjHPMIdkfr3tCY4GoO33xhlgAvLKV6HgosJcrluXLfGbCjJff1iDS2nrDX2ZVCnY47xdHDONbiEiPjCGj3F9nChxsOwuTL4uXO',
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2023-10-10",
+  token: process.env.SANITY_API_TOKEN, 
   useCdn: false,  
-  
 });
+
 
 
