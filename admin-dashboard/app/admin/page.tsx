69e808c0f01d9ba65b20a377f9d8deae7ecb1641
KhawajaAbdulMoiz <khawajaahmedraza4@gmail.com>
2025-02-07 19:48:35 +0500

    Admin Panel Created

--- admin-dashboard/app/admin/page.tsx
+++ admin-dashboard/app/admin/page.tsx
@@ -1,22 +1,22 @@
-// "use client";
+"use client";
 
-// import { useRouter } from "next/navigation";
-// import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
-// export default function AdminLogin() {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
+export default function AdminLogin() {
+  const [email] = useState("");
+  const [password] = useState("");
 
-//   const router = useRouter();
+  const router = useRouter();
 
-//   const handleLogin = (e: React.FormEvent) => {
-//     e.preventDefault();
+  const handleLogin = (e: React.FormEvent) => {
+    e.preventDefault();
 
-//     if (email === "abdulmoiz@gmail.com" && password === "khawaja") {
-//       localStorage.setItem("isLoggedIn", "true");
-//       router.push("/admin/dashboard");
-//     } else {
-//       alert("Invalid email or password");
-//     }
-//   };
-// }
+    if (email === "abdulmoiz@gmail.com" && password === "khawaja") {
+      localStorage.setItem("isLoggedIn", "true");
+      router.push("/admin/dashboard");
+    } else {
+      alert("Invalid email or password");
+    }
+  };
+}
--- admin-dashboard/app/page.tsx
+++ admin-dashboard/app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div>
-
+      <h1>Admin Dashboard</h1>
     </div>
   );
 }
