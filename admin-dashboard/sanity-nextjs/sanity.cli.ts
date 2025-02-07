e25cfd6f066f675b42038b128bc76eae72d9849a
KhawajaAbdulMoiz <khawajaahmedraza4@gmail.com>
2025-02-07 20:54:31 +0500

    Admion Panel Created

--- admin-dashboard/sanity-nextjs/sanity.cli.ts
+++ admin-dashboard/sanity-nextjs/sanity.cli.ts
@@ -1,8 +1,10 @@
+import { defineCliConfig } from 'sanity/cli';
 /**
 * This configuration file lets you run `$ sanity [command]` in this folder
 * Go to https://www.sanity.io/docs/cli to learn more.
 **/
-import { defineCliConfig } from 'sanity/cli'
+
+
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
@@ -13,3 +15,5 @@ export default defineCliConfig({
 
      } 
     })
+
+
